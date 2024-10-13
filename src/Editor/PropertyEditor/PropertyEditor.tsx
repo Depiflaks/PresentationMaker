4801b524994311a9adc5ce4789987817e91de034
Depiflaks <ssmirnov130405@gmail.com>
2024-10-13 12:56:04 +0300

    добавил изменение высоты Property Editor

--- src/Editor/PropertyEditor/PropertyEditor.tsx
+++ src/Editor/PropertyEditor/PropertyEditor.tsx
@@ -1,20 +1,41 @@
+import { useRef, useState } from "react";
 import { Slide } from "../../state/Types/types";
 import "./PropertyEditor.css";
+import Resizer from "./Resizer/Resizer";
 
 type Props = {
     slide: Slide
 }
 
+export const Interval: {min: number, max: number} = {
+    min: 150,
+    max: 500
+}
+
 export default function PropertyEditor({slide}: Props) {
+
+    const [separator, setSeparator] = useState<number>(Interval.min);
+    const propertiesRef = useRef<HTMLDivElement>(null);
+    const editorRef = useRef<HTMLDivElement>(null);
+    
+    const onSeparatorChange = (newSize: number) => {
+        setSeparator(Math.min(Math.max(Interval.min, newSize), Interval.max))
+    };
+
+    const conponentsHeight = editorRef.current ? editorRef.current!.getBoundingClientRect().height - separator - 120 : Interval.max;
+    
     return (
-        <div className="property-editor">
+        <div className="property-editor" ref={editorRef}>
             <h3>Properties</h3>
-            
-            <div className="properties-settings">
+            <div className="properties-settings" style={{height: `${separator}px`}} ref={propertiesRef}>
+                
                 {true ? <p>No element selected</p> : <></>}
             </div>
+
+            <Resizer onSeparatorChange={onSeparatorChange} propertiesRef={propertiesRef}/>
             <h3>Components</h3>
-            <div className="components-settings">
+            <div className="components-settings" style={{height: `${conponentsHeight}px`}}>
+                
                 {true ? <p>The list of components is empty</p> : <></>}
             </div>
         </div>
---
+++ src/Editor/PropertyEditor/Resizer/Resizer.tsx
@@ -0,0 +1,36 @@
+import React, { useRef } from "react"
+
+type Props = {
+    onSeparatorChange: (newSize: number) => void
+    propertiesRef: React.RefObject<HTMLDivElement>
+}
+
+export default function Resizer({onSeparatorChange, propertiesRef}: Props) {
+    const resizerRef = useRef<HTMLDivElement>(null);
+
+    const handleMouseDown = (event: React.MouseEvent) => {
+        event.preventDefault();
+
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    };
+
+    const handleMouseMove = (event: MouseEvent) => {
+        const newHeight = event.clientY - propertiesRef.current!.getBoundingClientRect().top - resizerRef.current!.getBoundingClientRect().height / 2 - 10;
+        onSeparatorChange(newHeight);
+    };
+
+    const handleMouseUp = () => {
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    return (
+        <div
+            className="resizer"
+            ref={resizerRef}
+            draggable={true}
+            onMouseDown={handleMouseDown}
+        />
+    )
+}
