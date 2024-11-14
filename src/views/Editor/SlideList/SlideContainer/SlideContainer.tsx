99ec2e49f0504a9a1538c5898308a1259535de6e
Smirnov Sergey <ssmirnov130405@gmail.com>
2024-11-14 20:21:40 +0300

    рефакторинг кода

---
+++ src/views/Editor/SlideList/SlideContainer/SlideContainer.tsx
@@ -0,0 +1,115 @@
+import SlidePreview from "~/views/Editor/SlideList/SlideContainer/SlidePreview/SlidePreview";
+import add from "~/views/assets/SlideList/add.svg"
+import SlideSeparator from "~/views/Editor/SlideList/SlideContainer/SlideSeparator/SlideSeparator";
+import MaketPanel from "~/views/Editor/SlideList/SlideContainer/MaketPanel/MaketPanel";
+import { createSlide } from "~/store/Methods/Slide/Slide";
+import { useRef, useState } from "react";
+import { Presentation } from "~/store/Types/types";
+import { dispatch } from "~/store/editor";
+import { changeCurrent, removeSlide, storeSlide } from "~/store/Methods/Presentation/Presentation";
+
+type Props = {
+    editor: Presentation;
+    dragEnterId: string;
+    setDragEnterId: (dragId: string) => void;
+}
+
+const endSlide = "endSlide";
+
+export default function SlideContainer({editor, dragEnterId, setDragEnterId}: Props) {
+    const {order, current, slides} = editor;
+    const slideArray = order.map((id) => slides[id]);
+    const [activeSeparator, setActiveSeparator] = useState<string>(endSlide);
+    const [isMaketPanelVisible, setIsMaketPanelVisible] = useState<boolean>(false);
+    const [panelPosition, setPanelPosition] = useState<{ top: number; left: number }>({ top: 0, left: 0 });
+    const addButtonRef = useRef<HTMLDivElement>(null);
+    const panelRef = useRef<HTMLDivElement>(null);
+
+    const onSlideClick = (id: string): void => {
+        dispatch(changeCurrent, id);
+    }
+
+    const onSeparatorClick = (id: string) => {
+        setActiveSeparator(id);
+    }
+
+    const onDragEnter = (id: string) => {
+        setDragEnterId(id);
+    }
+
+    const deleteSlide = (id: string) => {
+        dispatch(removeSlide, id);
+    }
+
+    const toggleMaketPanel = () => {
+        if (!isMaketPanelVisible && addButtonRef.current) {
+            const { top, left, width } = addButtonRef.current.getBoundingClientRect();
+            setPanelPosition({
+                top: Math.min(top, window.innerHeight - 150),
+                left: left + window.scrollX + width + 10,
+            });
+        }
+        setIsMaketPanelVisible((prev) => !prev);
+    };
+
+    const onMaketSelect = (index: number) => {
+        const newSlide = createSlide(index);
+        dispatch(storeSlide, newSlide);
+        setIsMaketPanelVisible(false);
+    };
+
+    return (
+    <div className="slide-container">
+        {slideArray.map((slide, i) => (
+            <div
+                key={i}
+            >
+                <SlideSeparator
+                    slideId={slide.id}
+                    isSelected={slide.id === activeSeparator}
+                    isEntered={slide.id === dragEnterId}
+                    onSeparatorClick={() => {onSeparatorClick(slide.id)}}
+                    onDragEnter={() => {onDragEnter(slide.id)}}
+                />
+                <SlidePreview 
+                    slide={slide} 
+                    isSelected={current === slide.id} 
+                    onSlideClick={() => onSlideClick(slide.id)}
+                    onDragEnter={() => {onDragEnter(slide.id)}}
+                    onDelete={() => {deleteSlide(slide.id)}}
+                />
+            </div>
+        ))}
+        <div style={{width: "100%"}}>
+            <SlideSeparator
+                key={endSlide}
+                slideId={endSlide}
+                isSelected={endSlide === activeSeparator}
+                isEntered={endSlide === dragEnterId}
+                onSeparatorClick={() => {onSeparatorClick(endSlide)}}
+                onDragEnter={() => {onDragEnter(endSlide)}}
+            />
+            <div className="add-button"
+                onDragEnter={(event) => {
+                    event.preventDefault()
+                    onDragEnter(endSlide)
+                }}
+                onDragOver={(event) => {event.preventDefault()}}
+                draggable={false}
+                onClick={toggleMaketPanel}
+                ref={addButtonRef}
+            >
+                <img src={add} draggable={false}/>
+            </div>
+        </div>
+        
+        {isMaketPanelVisible && <MaketPanel 
+            onSelect={onMaketSelect} 
+            style={{ top: `${panelPosition.top}px`, left: `${panelPosition.left}px` }} 
+            isVisible={isMaketPanelVisible}
+            onClose={() => {setIsMaketPanelVisible(false);}}
+            panelRef={panelRef}
+        />}
+    </div>
+    )
+}
--- src/views/Editor/SlideList/SlideList.tsx
+++ src/views/Editor/SlideList/SlideList.tsx
@@ -1,13 +1,12 @@
 import { Presentation } from "~/store/Types/types";
 import "./SlideList.css";
-import SlidePreview from "./SlidePreview/SlidePreview";
-import add from "~/views/assets/SlideList/add.svg"
-import { changeCurrent, moveSlide, removeSlide, storeSlide } from "~/store/Methods/Presentation/Presentation";
-import SlideSeparator from "./SlideSeparator/SlideSeparator";
-import { useRef, useState } from "react";
+
+import { moveSlide } from "~/store/Methods/Presentation/Presentation";
+
+import { useState } from "react";
 import { dispatch } from "~/store/editor";
-import MaketPanel from "./MaketPanel/MaketPanel";
-import { createSlide } from "~/store/Methods/Slide/Slide";
+import SlideContainer from "~/views/Editor/SlideList/SlideContainer/SlideContainer";
+
 
 type Props = {
     editor: Presentation
@@ -16,56 +15,14 @@ type Props = {
 const endSlide = "endSlide"
 
 export default function SlideList({editor}: Props) {
-    const {order, current, slides} = editor;
-
-    const [activeSeparator, setActiveSeparator] = useState<string>(endSlide);
+    const {order, current} = editor;
     const [dragEnterId, setDragEnterId] = useState<string>('');
-    const [isMaketPanelVisible, setIsMaketPanelVisible] = useState<boolean>(false);
-    const [panelPosition, setPanelPosition] = useState<{ top: number; left: number }>({ top: 0, left: 0 });
-    const addButtonRef = useRef<HTMLDivElement>(null);
-    const panelRef = useRef<HTMLDivElement>(null);
-
-    const slideArray = order.map((id) => slides[id]);
-
-    const onSlideClick = (id: string): void => {
-        dispatch(changeCurrent, id);
-    }
-
-    const onSeparatorClick = (id: string) => {
-        setActiveSeparator(id);
-    }
-
-    const onDragEnter = (id: string) => {
-        setDragEnterId(id);
-    }
-
+    
     const onDrop = () => {
         const newIndex = dragEnterId === endSlide ? order.length : order.indexOf(dragEnterId);
         dispatch(moveSlide, {slideId: current, newIndex: newIndex});
         setDragEnterId("");
     }
-
-    const deleteSlide = (id: string) => {
-        dispatch(removeSlide, id);
-    }
-
-    const toggleMaketPanel = () => {
-        if (!isMaketPanelVisible && addButtonRef.current) {
-            const { top, left, width, height } = addButtonRef.current.getBoundingClientRect();
-            setPanelPosition({
-                top: Math.min(top, window.innerHeight - 150),
-                left: left + window.scrollX + width + 10,
-            });
-        }
-        setIsMaketPanelVisible((prev) => !prev);
-    };
-
-    const onMaketSelect = (index: number) => {
-        const newSlide = createSlide(index);
-        console.log(index);
-        dispatch(storeSlide, newSlide);
-        setIsMaketPanelVisible(false);
-    };
 
     return (
         <div className="slide-list"
@@ -76,58 +33,11 @@ export default function SlideList({editor}: Props) {
             }}
         >
             <h3>Slides</h3>
-            <div className="slide-container">
-                {slideArray.map((slide, i) => (
-                    <div
-                        key={i}
-                    >
-                        <SlideSeparator
-                            slideId={slide.id}
-                            isSelected={slide.id === activeSeparator}
-                            isEntered={slide.id === dragEnterId}
-                            onSeparatorClick={() => {onSeparatorClick(slide.id)}}
-                            onDragEnter={() => {onDragEnter(slide.id)}}
-                        />
-                        <SlidePreview 
-                            slide={slide} 
-                            isSelected={current === slide.id} 
-                            onSlideClick={() => onSlideClick(slide.id)}
-                            onDragEnter={() => {onDragEnter(slide.id)}}
-                            onDelete={() => {deleteSlide(slide.id)}}
-                        />
-                    </div>
-                ))}
-                <div style={{width: "100%"}}>
-                    <SlideSeparator
-                        key={endSlide}
-                        slideId={endSlide}
-                        isSelected={endSlide === activeSeparator}
-                        isEntered={endSlide === dragEnterId}
-                        onSeparatorClick={() => {onSeparatorClick(endSlide)}}
-                        onDragEnter={() => {onDragEnter(endSlide)}}
-                    />
-                    <div className="add-button"
-                        onDragEnter={(event) => {
-                            event.preventDefault()
-                            onDragEnter(endSlide)
-                        }}
-                        onDragOver={(event) => {event.preventDefault()}}
-                        draggable={false}
-                        onClick={toggleMaketPanel}
-                        ref={addButtonRef}
-                    >
-                        <img src={add} draggable={false}/>
-                    </div>
-                </div>
-                
-                {isMaketPanelVisible && <MaketPanel 
-                    onSelect={onMaketSelect} 
-                    style={{ top: `${panelPosition.top}px`, left: `${panelPosition.left}px` }} 
-                    isVisible={isMaketPanelVisible}
-                    onClose={() => {setIsMaketPanelVisible(false);}}
-                    panelRef={panelRef}
-                />}
-            </div>
+            <SlideContainer
+                editor={editor}
+                dragEnterId={dragEnterId}
+                setDragEnterId={setDragEnterId}
+            />
         </div>
     );
 };
